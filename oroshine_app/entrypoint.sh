6e382ed0c09b0d43c7947ede832f9c2459d7e763
nikhilchandurkar <nikhilchandurkar24@gmail.com>
2025-12-09 14:07:42 +0530

    fix entrypoint shell script

--- oroshine_app/entrypoint.sh
+++ oroshine_app/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Wait for PostgreSQL to be ready
 echo "Waiting for PostgreSQL..."
-MAX_ATTEMPTS=50
+MAX_ATTEMPTS=15
 COUNT=0
 until nc -z $PG_HOST $PG_PORT; do
   COUNT=$((COUNT+1))
@@ -14,7 +14,7 @@ until nc -z $PG_HOST $PG_PORT; do
   sleep 0.5
 done
 echo "PostgreSQL started successfully"
-
+python manage.py makemigrations --noinput
 echo "===================================="
 echo "Applying Migrations..."
 python manage.py migrate --noinput
@@ -24,8 +24,13 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 echo "===================================="
 
+# echo "compressing static files..."
+# python manage.py compress --force 
+# echo "===================================="
+
+
 echo "Starting Production Server (Gunicorn)..."
-exec gunicorn oroshine.wsgi:application \
+exec gunicorn oroshine_app.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 3 \
     --access-logfile - \
