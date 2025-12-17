c3461c87952ff6c2562ecd0a703eb070ff0e4a55
nikhilchandurkar <nikhilchandurkar24@gmail.com>
2025-12-18 00:02:28 +0530

    fixed docker and docker compose file

--- oroshine_app/entrypoint.sh
+++ oroshine_app/entrypoint.sh
@@ -1,36 +1,3 @@
-# #!/bin/bash
-# set -e
-
-# echo "Waiting for PostgreSQL..."
-# while ! nc -z "$PG_HOST" "$PG_PORT"; do
-#   sleep 0.5
-# done
-
-# echo "PostgreSQL is up."
-
-# python manage.py makemigrations --noinput
-# python manage.py migrate --noinput
-
-# echo "Collecting static files..."
-# python manage.py collectstatic --noinput
-
-# echo "Compressing static files..."
-# python manage.py compress --force || true
-
-# echo "Starting gunicorn..."
-# exec gunicorn oroshine_app.wsgi:application \
-#   --bind 0.0.0.0:8000 \
-#   --workers 3 \
-#   --timeout 120 \
-#   --access-logfile - \
-#   --error-logfile -
-
-
-
-
-
-
-
 #!/bin/bash
 set -e
 
@@ -46,22 +13,22 @@ echo "Running migrations..."
 python manage.py migrate --noinput
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput 
+python manage.py collectstatic --noinput
 
-echo "compressing static files..."
-python manage.py compress --force  
+echo "Compressing static files..."
+python manage.py compress --force
 
-echo "Starting gunicorn with optimized settings..."
+echo "Starting Gunicorn..."
 exec gunicorn oroshine_app.wsgi:application \
-  --bind 0.0.0.0:8000 \
-  --workers 2 \
-  --threads 4\
-  --worker-class gthread \
-  --worker-tmp-dir /dev/shm \
-  --timeout 60 \
-  --graceful-timeout 30 \
-  --max-requests 400 \
-  --max-requests-jitter 50 \
-  --access-logfile - \
-  --error-logfile - \
-  --log-level warning
+    --bind 0.0.0.0:8000 \
+    --workers 2 \
+    --threads 2 \
+    --worker-class gthread \
+    --worker-tmp-dir /dev/shm \
+    --timeout 60 \
+    --graceful-timeout 30 \
+    --max-requests 200 \
+    --max-requests-jitter 50 \
+    --access-logfile - \
+    --error-logfile - \
+    --log-level warning
