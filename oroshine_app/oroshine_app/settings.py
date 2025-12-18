b93bf732fc605b234ab60347061da1f01e1296c5
nikhilchandurkar <nikhilchandurkar24@gmail.com>
2025-12-18 22:33:21 +0530

    Disable SSL for IP-based deployment

--- oroshine_app/oroshine_app/settings.py
+++ oroshine_app/oroshine_app/settings.py
@@ -22,15 +22,15 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1',
                        cast=lambda v: [s.strip() for s in v.split(',')])
 
 # Security enhancements
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = False     # true when add ss certificate to redirect to https port all below 
+SESSION_COOKIE_SECURE = False 
+CSRF_COOKIE_SECURE = False 
+# SECURE_BROWSER_XSS_FILTER = 
+# SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
+# SECURE_HSTS_SECONDS = 31536000
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
 
 # ==========================================
 # APPLICATION DEFINITION
